0de1cefe3d98ac670593fe9b38faac164ca17976
Jacob Carlborg <doob@me.com>
2022-08-09 20:29:43 +0200

    fixup! Add support for OpenBSD on ARM64

--- dist/index.js
+++ dist/index.js
@@ -1487,9 +1487,9 @@ const version = {
     operating_system: {
         freebsd: 'v0.2.0',
         netbsd: 'v0.0.1',
-        openbsd: 'v0.3.0'
+        openbsd: 'v0.4.0-rc4'
     },
-    resources: 'v0.6.0-rc3'
+    resources: 'v0.6.0-rc5'
 };
 exports.default = version;
 //# sourceMappingURL=version.js.map
--- src/version.ts
+++ src/version.ts
@@ -2,10 +2,10 @@ const version = {
   operating_system: {
     freebsd: 'v0.2.0',
     netbsd: 'v0.0.1',
-    openbsd: 'v0.3.0'
+    openbsd: 'v0.4.0-rc4'
   },
 
-  resources: 'v0.6.0'
+  resources: 'v0.6.0-rc5'
 }
 
 export default version
