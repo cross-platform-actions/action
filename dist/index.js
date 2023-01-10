2987461bda2c2ad9a202df479e3da6ea00ce4940
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2023-01-10 12:53:51 -0600

    ci: update 0.4.0 to 0.5.0

--- dist/index.js
+++ dist/index.js
@@ -1876,7 +1876,7 @@ const version = {
     operating_system: {
         freebsd: 'v0.2.0',
         netbsd: 'v0.0.1',
-        openbsd: 'v0.4.0'
+        openbsd: 'v0.5.0'
     },
     resources: 'v0.6.0'
 };
