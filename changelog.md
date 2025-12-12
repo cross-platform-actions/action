1ec539ef8d2c52533c9080254650d10be660dce0
Jacob Carlborg <doob@me.com>
2025-12-12 18:51:54 +0100

    Fix #113: empty hostname on FreeBSD and NetBSD
    This also sets a fully qualified and consistent hostname on all
    platforms. Note, for some older versions on some platforms it's not
    possible to rebuild the VM images anymore. Therefore the issue remains
    on those versions.

--- changelog.md
+++ changelog.md
@@ -8,6 +8,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - Add support for FreeBSD 15.0 ([#114](https://github.com/cross-platform-actions/action/issues/114))
 
+### Fixed
+- Fix empty hostname ([#113](https://github.com/cross-platform-actions/action/issues/113))
+
 ## [0.30.0] - 2025-11-04
 ### Added
 - Document how to report a security vulnerability
--- dist/index.js
+++ dist/index.js
@@ -2504,10 +2504,10 @@ exports.ExecExecutor = ExecExecutor;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const version = {
     operating_system: {
-        freebsd: 'v0.13.0',
-        haiku: 'v0.0.1',
-        netbsd: 'v0.5.0',
-        openbsd: 'v0.11.0'
+        freebsd: 'v0.13.1',
+        haiku: 'v0.0.2',
+        netbsd: 'v0.5.1',
+        openbsd: 'v0.11.1'
     },
     resources: 'v0.11.0'
 };
